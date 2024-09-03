e7e8696b104db19f85c5ccbac2a724e8095fca89
hpham474 <hpham474@gmail.com>
2024-09-02 23:25:26 -0500

    style basic layout

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import "../styles/Card.css";
+
+function Card() {
+  return (
+    <>
+      <div className="card">
+        <div className="placeholder"></div>
+        <p className="name">Placeholder Image </p>
+      </div>
+    </>
+  );
+}
+
+export default Card;
