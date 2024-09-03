34fbca896b14e7162f8cdf4f828dc3de8f690e7f
hpham474 <hpham474@gmail.com>
2024-09-03 04:54:22 -0500

    fetch and display data from pokemon api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,34 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import "../styles/Card.css";
 
-function Card() {
+function Card({ id }) {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("https://pokeapi.co/api/v2/pokemon/4");
+      const json = await data.json();
+
+      setPokemon(json);
+    };
+
+    fetchData().catch(console.error);
+  }, []);
+
   return (
     <>
       <div className="card">
-        <div className="placeholder"></div>
-        <p className="name">Placeholder Image </p>
+        {/* <div className="placeholder"></div> */}
+        <img src={pokemon.sprites.front_default}></img>
+        <p className="name">{pokemon === null ? "loading..." : pokemon.name}</p>
       </div>
     </>
   );
 }
+
+Card.propTypes = {
+  id: PropTypes.string,
+};
 
 export default Card;
